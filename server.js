a88d9fe61ebf2912822e41737d993a9293cb6d36
PPL <fhbj18185@gmail.com>
2025-10-26 11:02:43 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PPLnwza007</title>
+  <style>
+    /* ทำให้ข้อความอยู่กลางหน้าจอ */
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh; /* เต็มหน้าจอ */
+      margin: 0;
+      background-color: #f0f0f0; /* สีพื้นหลัง */
+      font-family: Arial, sans-serif;
+    }
+
+    h1 {
+      font-size: 150px; /* ขนาดใหญ่ */
+      color: #ff0000;   /* สีแดง */
+    }
+  </style>
+</head>
+<body>
+  <h1>5-0</h1>
+</body>
+</html>
